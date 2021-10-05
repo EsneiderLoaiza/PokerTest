72e583ca2644f0cc0c39e3606568838cf3abe7bf
esneider-ies <esneider-ies>
2021-10-05 09:39:08 -0500

    feat: Se implementa cartaAlta como prueba para test, otros métodos con if para saber si son iguales y saltar a cartaAlta

--- src/main/java/domain/impl/ManosPokerImpl.java
+++ src/main/java/domain/impl/ManosPokerImpl.java
@@ -1,20 +1,31 @@
 package domain.impl;
 
+import domain.Card;
 import domain.Player;
 import domain.interfaces.IManosPoker;
+
 
 public class ManosPokerImpl implements IManosPoker {
 
 
     public String cartaAlta(Player player1, Player player2) {
-        return null;
+         if(player1.hand == player2.hand) {
+            return "Prueba";
+         }
+        return "Prueba";
     }
 
     public String par(Player player1, Player player2) {
+        if(player1.hand.equals(player2.hand)) {
+            return cartaAlta(player1, player2);
+        }
         return null;
     }
 
     public String dosPares(Player player1, Player player2) {
+        if(player1.hand.equals(player2.hand)) {
+            return cartaAlta(player1, player2);
+        }
         return null;
     }
 }
