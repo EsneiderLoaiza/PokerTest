1433e8a5a3c5ab9400b3980b47e5f186f90ba17e
esneider-ies <esneider-ies>
2021-10-05 09:33:35 -0500

    feat: Se crea atributo de tipo Hand con su constructor

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,21 +1,10 @@
 package domain;
 
-import domain.enums.Palo;
-
-import java.util.List;
-
 public class Player {
 
+    public Hand hand;
 
-    /*Card card1 = new Card(2, Palo.C);
-    Card card2 = new Card(3, Palo.D);
-    Card card3 = new Card(4, Palo.H);
-    Card card4 = new Card(5, Palo.S);
-    Card card5 = new Card(6, Palo.C);*/
-
-    List<Card> cartas;
-
-    public Player(List<Card> cartas) {
-        this.cartas = cartas;
+    public Player(Hand hand) {
+        this.hand = hand;
     }
 }
