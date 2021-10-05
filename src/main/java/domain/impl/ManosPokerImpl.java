0f73a0ad77690329e72fc9a570edbc1acc02eced
esneider-ies <esneider-ies>
2021-10-05 18:13:06 -0500

    Se implementa ejemplo cartaAlta

--- src/main/java/domain/impl/ManosPokerImpl.java
+++ src/main/java/domain/impl/ManosPokerImpl.java
@@ -9,10 +9,17 @@ public class ManosPokerImpl implements IManosPoker {
 
 
     public String cartaAlta(Player player1, Player player2) {
-         if(player1.hand == player2.hand) {
+        Card[] cardsPlayer1 = player1.hand.getHand();
+        int valuePlayer1 = countCartas(cardsPlayer1);
+        int valuePlayer2 = countCartas(cardsPlayer1);
+         if(cardsPlayer1[0].getValorCarta() == cardsPlayer1[0].getValorCarta()) {
             return "Prueba";
          }
         return "Prueba";
+    }
+
+    private int countCartas(Card[] cartas){
+        return  1; //Cuenta cartas para sacar valor de cada mano
     }
 
     public String par(Player player1, Player player2) {
