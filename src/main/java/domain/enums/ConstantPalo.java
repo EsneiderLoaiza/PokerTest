36f012949e3af236e2dd49d1f8fd9fe9bdbc3fd7
esneider-ies <esneider-ies>
2021-10-05 09:32:43 -0500

    feat: Se crea ConstantPalo como opción para el uso de los mazos (Ya se tiene un enum de esto)

---
+++ src/main/java/domain/enums/ConstantPalo.java
@@ -0,0 +1,10 @@
+package domain.enums;
+
+public class PaloConstant {
+
+    final private static int C = 11;
+    final private static int D = 12;
+    final private static int H = 13;
+    final private static int S = 14;
+
+}
