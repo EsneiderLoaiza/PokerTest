926f7e96472e9fc538918a82cbd06ec4b37a481b
esneider-ies <esneider-ies>
2021-10-06 09:54:10 -0500

    feat: Se implementa método par con método compareCards

--- src/main/java/domain/impl/ManosPokerImpl.java
+++ src/main/java/domain/impl/ManosPokerImpl.java
@@ -23,19 +23,38 @@ public class ManosPokerImpl implements IManosPoker {
 
     public int countCartas(Card[] cartas, Player player){
         int totalValue = 0;
-        int totalValuePalo = 0;
         for(int i=0; i<5; i++) {
             totalValue = totalValue + cartas[i].getValorCarta() + cartas[i].getPalo().getPaloValue();
-            //totalValuePalo = totalValuePalo + cartas[i].getPalo().getPaloValue();
         }
         return  totalValue;
     }
 
-    public String par(Player player1, Player player2) {
-        if(player1.hand.equals(player2.hand)) {
-            return cartaAlta(player1, player2);
+    public int compareCards(Card[] cartas, Player player) {
+        int previousValue = 0;
+        int repeatedValue = 0;
+        for(int i=0; i<5; i++) {
+            previousValue = cartas[i].getValorCarta();
+            for (int j=0; j<5; j++) {
+                if(previousValue == cartas[i+1].getValorCarta()) {
+                    repeatedValue = previousValue;
+                    return  repeatedValue;
+                }
+            }
         }
-        return null;
+        return 0;
+    }
+
+    public String par(Player player1, Player player2) {
+        Card[] cardsPlayer1 = player1.hand.getHand();
+        Card[] cardsPlayer2 = player2.hand.getHand();
+
+        int player1Value = compareCards(cardsPlayer1, player1);
+        int player2Value = compareCards(cardsPlayer2, player2);
+
+        if(player1Value > player2Value) {
+            return "Jugador 1 gana por par más alto";
+        }
+        return "Jugador 2 gana por par más alto";
     }
 
     public String dosPares(Player player1, Player player2) {
