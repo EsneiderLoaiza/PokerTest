7f270b24cad56b644c597e01acb9a51e0adc7dd7
esneider-ies <esneider-ies>
2021-10-08 17:45:07 -0500

    feat: Se genera enum de ValueCards para dar valor a cada carta

--- src/main/java/domain/enums/ValueCard.java
+++ src/main/java/domain/enums/ValueCard.java
@@ -1,2 +1,32 @@
-package domain.enums;public enum ValueCard {
+package domain.enums;
+
+public enum ValueCard {
+
+    DOS(2),
+    TRES(3),
+    CUATRO(4),
+    CINCO(5),
+    SEIS(6),
+    SIETE(7),
+    OCHO(8),
+    NUEVE(9),
+    DIEZ(10),
+    JOTA(11),
+    QUEEN(12),
+    KING(13),
+    AS(14);
+
+    private int valueCard;
+
+    ValueCard(int valueCard) {
+        this.valueCard = valueCard;
+    }
+
+    public int getValueCard() {
+        return valueCard;
+    }
+
+    public void setValueCard(int valueCard) {
+        this.valueCard = valueCard;
+    }
 }
