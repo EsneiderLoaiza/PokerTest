4aca5d48e385c4b3d367bb56ac34f40b4e2fb681
esneider-ies <esneider-ies>
2021-10-08 17:45:41 -0500

    feat: Se agregan dos nuevos métodos a impelmentar, terna y escalera

--- src/main/java/domain/interfaces/IManosPoker.java
+++ src/main/java/domain/interfaces/IManosPoker.java
@@ -5,12 +5,16 @@ import domain.Player;
 
 public interface IManosPoker {
 
-    public int countCartas(Card[] cartas, Player player);
+    public int countCartas(Card[] cartas);
 
     public String cartaAlta(Player player1, Player player2);
 
     public String par(Player player1, Player player2);
 
     public String dosPares(Player player1, Player player2);
+
+    public String terna(Player player1, Player player2);
+
+    public String escalera(Player player1, Player player2);
 
 }
