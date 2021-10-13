736f5188128303391b23413ad7fd318c05fbcbbb
esneider-ies <esneider-ies>
2021-10-13 08:53:06 -0500

    feat: nuevo atributo para mensaje ganador en clase player, con métodos get y set

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -3,8 +3,17 @@ package domain;
 public class Player {
 
     public Hand hand;
+    private String winnerMessage;
 
     public Player(Hand hand) {
         this.hand = hand;
+    }
+
+    public String getWinnerMessage() {
+        return winnerMessage;
+    }
+
+    public void setWinnerMessage(String winnerMessage) {
+        this.winnerMessage = winnerMessage;
     }
 }
