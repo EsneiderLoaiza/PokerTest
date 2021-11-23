2541ae34a3a45f89b78a0a467d846021936dbc30
EsneiderLoaiza <esneiderlvs@gmail.com>
2021-11-22 21:51:15 -0500

    feat(constants): Se agrega clase constante para cada mano de poker ganadora

---
+++ src/main/java/domain/constants/HandsConstants.java
@@ -0,0 +1,14 @@
+package domain.constants;
+
+public class HandsConstants {
+
+    public final static String CARTA_ALTA = "carta alta";
+    public final static String PAR = "par mas alto";
+    public final static String DOS_PARES = "dos pares mas alto";
+    public final static String TERNA = "terna mas alta";
+    public final static String ESCALERA = "escalera mas alta";
+    public final static String COLOR = "valor mas alto con color";
+    public final static String FULL_HOUSE = "full house";
+    public final static String POKER = "poker mas alto";
+    public final static String ESCALERA_DE_COLOR = "escalera de color";
+}
